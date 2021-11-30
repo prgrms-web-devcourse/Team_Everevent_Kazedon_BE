bd97af0f25c1ccb9f9aff56b68944c8868bd5ecd
heoseungyeon <dia0312@naver.com>
2021-11-30 15:50:27 +0900

    feat: spring 프로젝트생성

---
+++ src/main/java/com/example/Everevent/EvereventApplication.java
@@ -0,0 +1,13 @@
+package com.example.Everevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvereventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvereventApplication.class, args);
+	}
+
+}
