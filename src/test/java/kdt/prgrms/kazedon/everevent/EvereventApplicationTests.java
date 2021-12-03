0f1ab47c33e2cbfa869be15b88f0bdaed7686503
nasaoreo <nasaoreo121@gmail.com>
2021-12-03 18:30:51 +0900

    feat: 프로젝트 환경 설정
    - CI 및 docker 설정
    - gradle dependency 설정
    - .gitignore 설정

---
+++ src/test/java/kdt/prgrms/kazedon/everevent/EvereventApplicationTests.java
@@ -0,0 +1,13 @@
+package kdt.prgrms.kazedon.everevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvereventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
