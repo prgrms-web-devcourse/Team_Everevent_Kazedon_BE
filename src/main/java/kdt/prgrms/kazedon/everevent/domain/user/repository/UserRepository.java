a650eaca26ee333ec20547d51e584ea3e4980724
Heejeong01110 <123rkdrkd@naver.com>
2021-12-05 23:06:02 +0900

    feat: 로그인, 회원가입, 로그아웃 구현

---
+++ src/main/java/kdt/prgrms/kazedon/everevent/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package kdt.prgrms.kazedon.everevent.domain.user.repository;
+
+import java.util.Optional;
+import kdt.prgrms.kazedon.everevent.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
+
+}
