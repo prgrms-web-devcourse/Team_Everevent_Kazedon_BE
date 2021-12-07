26060af77574729552675a99192b9e4282fb6ab3
nasaoreo <nasaoreo121@gmail.com>
2021-12-07 23:57:35 +0900

    feat: GlobalExceptionHandler 클래스 추가

---
+++ src/main/java/kdt/prgrms/kazedon/everevent/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package kdt.prgrms.kazedon.everevent.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
