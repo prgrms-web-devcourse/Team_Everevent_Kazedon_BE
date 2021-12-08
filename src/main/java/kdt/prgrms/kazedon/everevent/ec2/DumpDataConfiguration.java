b1c12449943c673bf7783ac6a5f23045dea0d33e
nasaoreo <81736440+nasaoreo@users.noreply.github.com>
2021-12-08 17:31:48 +0900

    test: ec2에 데이터 추가를 위한 config 파일 추가
    -  test: ec2에 데이터 추가를 위한 config 파일 추가
    - fix: DumpDataConfiguration 생성 오류 해결

---
+++ src/main/java/kdt/prgrms/kazedon/everevent/ec2/DumpDataConfiguration.java
@@ -0,0 +1,66 @@
+package kdt.prgrms.kazedon.everevent.ec2;
+
+import kdt.prgrms.kazedon.everevent.domain.event.Event;
+import kdt.prgrms.kazedon.everevent.domain.event.repository.EventRepository;
+import kdt.prgrms.kazedon.everevent.domain.market.Market;
+import kdt.prgrms.kazedon.everevent.domain.market.repository.MarketRepository;
+import kdt.prgrms.kazedon.everevent.domain.user.Authority;
+import kdt.prgrms.kazedon.everevent.domain.user.User;
+import kdt.prgrms.kazedon.everevent.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@RequiredArgsConstructor
+public class DumpDataConfiguration implements CommandLineRunner {
+    private final EventRepository eventRepository;
+    private final UserRepository userRepository;
+    private final MarketRepository marketRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user = User.builder()
+                .email("test@gmail.com")
+                .password("test-password")
+                .nickname("test-nickname")
+                .location("test-user-location")
+                .build();
+
+        Authority role_user = Authority.builder()
+                .user(user)
+                .authorityName("ROLE_USER")
+                .build();
+        user.addAuthority(role_user);
+        userRepository.save(user);
+
+        Market market = Market.builder()
+                .user(user)
+                .name("test-market-name")
+                .description("test-market-description")
+                .address("test-market-adderess")
+                .build();
+
+        marketRepository.save(market);
+
+        eventRepository.save(Event.builder()
+                .market(market)
+                .name("test-event")
+                .expiredAt(LocalDateTime.now().plusDays(3))
+                .description("this is a test event. nice to meet you!")
+                .maxParticipants(5)
+                .build()
+        );
+
+        eventRepository.save(Event.builder()
+                .market(market)
+                .name("test-another-event")
+                .expiredAt(LocalDateTime.now().plusDays(3))
+                .description("this is a test event. nice to meet you:)")
+                .maxParticipants(3)
+                .build()
+        );
+    }
+}
