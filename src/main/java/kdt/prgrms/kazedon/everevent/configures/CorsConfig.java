d449281b2db11a307c21fc4cd212bc19f7505034
heoseungyeon <dia0312@naver.com>
2021-12-08 12:24:25 +0900

    feat: WebSecurityconfig에서 CORS 설정

---
+++ src/main/java/kdt/prgrms/kazedon/everevent/configures/CorsConfig.java
@@ -0,0 +1,25 @@
+package kdt.prgrms.kazedon.everevent.configures;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsFilter corsFilter(){
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("http://localhost:3000");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+    config.addExposedHeader("X-AUTH-TOKEN");
+    source.registerCorsConfiguration("/api/v1/**", config);
+
+    return new CorsFilter(source);
+  }
+}
