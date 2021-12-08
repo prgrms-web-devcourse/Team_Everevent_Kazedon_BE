83d8a150323d3ccc01ae355d815f76352919e995
nasaoreo <nasaoreo121@gmail.com>
2021-12-08 17:19:43 +0900

    test: ec2에 데이터 추가를 위한 config 파일 추가

---
+++ src/main/java/kdt/prgrms/kazedon/everevent/domain/market/repository/MarketRepository.java
@@ -0,0 +1,7 @@
+package kdt.prgrms.kazedon.everevent.domain.market.repository;
+
+import kdt.prgrms.kazedon.everevent.domain.market.Market;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarketRepository extends JpaRepository<Market, Long> {
+}
