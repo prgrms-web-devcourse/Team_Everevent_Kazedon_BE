6664ccce580223ae7e034220d4fd4654abf5e4af
nasaoreo <nasaoreo121@gmail.com>
2021-12-11 21:13:10 +0900

    feat: 마켓 생성, 조회 기능 추가

--- src/main/java/kdt/prgrms/kazedon/everevent/domain/market/repository/MarketRepository.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/domain/market/repository/MarketRepository.java
@@ -1,7 +1,16 @@
 package kdt.prgrms.kazedon.everevent.domain.market.repository;
 
 import kdt.prgrms.kazedon.everevent.domain.market.Market;
+import kdt.prgrms.kazedon.everevent.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarketRepository extends JpaRepository<Market, Long> {
+    @Query("select m from Market m where m.user = :user")
+    Page<Market> findByUser(@Param("user") User user, Pageable pageable);
 }
