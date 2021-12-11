7abe9889464d183b75728c024bb8b35a0f89c110
nasaoreo <nasaoreo121@gmail.com>
2021-12-11 21:10:59 +0900

    fix: SignUpRequest의 생성자 추가
    - Lombok @NoArgsConstructor 어노테이션 사용

--- src/main/java/kdt/prgrms/kazedon/everevent/domain/user/dto/SignUpRequest.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/domain/user/dto/SignUpRequest.java
@@ -3,10 +3,12 @@ package kdt.prgrms.kazedon.everevent.domain.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignUpRequest {
 
   private String email;
