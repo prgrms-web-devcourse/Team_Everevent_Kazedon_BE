87817d8d432f17206d0120b395d2d7fbb1afa6f8
nasaoreo <nasaoreo121@gmail.com>
2021-12-11 21:13:10 +0900

    feat: 마켓 생성, 조회 기능 추가

--- src/main/java/kdt/prgrms/kazedon/everevent/exception/GlobalExceptionHandler.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/exception/GlobalExceptionHandler.java
@@ -1,7 +1,23 @@
 package kdt.prgrms.kazedon.everevent.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<String> handleNotFound(NotFoundException exception) {
+        log.warn(exception.getMessage());
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException exception){
+        log.warn(exception.getMessage());
+        return ResponseEntity.badRequest().build();
+    }
 }
---
+++ src/main/java/kdt/prgrms/kazedon/everevent/service/converter/MarketConverter.java
@@ -0,0 +1,37 @@
+package kdt.prgrms.kazedon.everevent.service.converter;
+
+import kdt.prgrms.kazedon.everevent.domain.market.dto.MarketCreateRequest;
+import kdt.prgrms.kazedon.everevent.domain.market.dto.MarketReadResponse;
+import kdt.prgrms.kazedon.everevent.domain.market.dto.SimpleMarket;
+import kdt.prgrms.kazedon.everevent.domain.market.Market;
+import kdt.prgrms.kazedon.everevent.domain.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketConverter {
+    public MarketReadResponse convertToMarketReadResponse(Page<SimpleMarket> simpleMarkets){
+        return MarketReadResponse.builder()
+                .simpleMarkets(simpleMarkets)
+                .build();
+    }
+
+    public SimpleMarket convertToSimpleMarket(Market market, int eventCount, int reviewCount){
+        return SimpleMarket.builder()
+                .marketId(market.getId())
+                .description(market.getDescription())
+                .eventCount(eventCount)
+                .favoriteCount(market.getFavoriteCount())
+                .reviewCount(reviewCount)
+                .build();
+    }
+
+    public Market convertToMarket(MarketCreateRequest createRequest, User user){
+        return Market.builder()
+                .user(user)
+                .name(createRequest.getName())
+                .description(createRequest.getDescription())
+                .address(createRequest.getDescription())
+                .build();
+    }
+}
