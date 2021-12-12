cd772c4a5c334e7b64e472444fab250d59537e08
nasaoreo <nasaoreo121@gmail.com>
2021-12-12 15:38:55 +0900

    feat: 이벤트 생성 기능 추가

--- src/main/java/kdt/prgrms/kazedon/everevent/exception/ErrorMessage.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package kdt.prgrms.kazedon.everevent.exception;
 
 public enum ErrorMessage {
   EVENT_NOT_FOUNDED("아이디 {0}에 해당하는 이벤트 정보가 없습니다."),
-  USER_NOT_FOUNDED("이메일 {0} 사용자 정보가 없습니다.");
+  USER_NOT_FOUNDED("이메일 {0} 사용자 정보가 없습니다."),
+  MARKET_NOT_FOUNDED("아이디 {0}에 해당하는 가게 정보가 없습니다");
 
   private final String message;
 
