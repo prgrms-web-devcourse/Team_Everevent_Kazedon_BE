08341ddab5513b4e1fd4b55eb8fe159e60982c03
nasaoreo <nasaoreo121@gmail.com>
2021-12-12 15:38:55 +0900

    feat: 이벤트 생성 기능 추가

--- src/main/java/kdt/prgrms/kazedon/everevent/controller/EventController.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/controller/EventController.java
@@ -1,15 +1,21 @@
 package kdt.prgrms.kazedon.everevent.controller;
 
+import kdt.prgrms.kazedon.everevent.configures.auth.AuthUser;
 import kdt.prgrms.kazedon.everevent.domain.event.dto.DetailEventReadResponse;
+import kdt.prgrms.kazedon.everevent.domain.event.dto.EventCreateRequest;
 import kdt.prgrms.kazedon.everevent.domain.event.dto.SimpleEventReadResponse;
+import kdt.prgrms.kazedon.everevent.domain.user.User;
 import kdt.prgrms.kazedon.everevent.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,17 +26,24 @@ public class EventController {
     @GetMapping("events")
     public ResponseEntity<SimpleEventReadResponse> getEvents(@RequestParam String location,
                                                              @PageableDefault(size=20, sort="expiredAt", direction = Sort.Direction.DESC) Pageable pageable){
-        return new ResponseEntity<>(
-                eventService.getEventsByLocation(location, pageable),
-                HttpStatus.OK
-        );
+        return ResponseEntity.ok(eventService.getEventsByLocation(location, pageable));
     }
 
     @GetMapping("events/{eventId}")
     public ResponseEntity<DetailEventReadResponse> getEvent(@PathVariable("eventId") Long eventId){
-        return new ResponseEntity<>(
-                eventService.getEventById(eventId),
-                HttpStatus.OK
-        );
+        return ResponseEntity.ok(eventService.getEventById(eventId));
+    }
+
+    @PostMapping("events")
+    public ResponseEntity<Long> createEvent(@Valid @RequestBody EventCreateRequest eventCreateRequest,
+                                            @AuthUser User user){
+        Long eventId = eventService.createEvent(eventCreateRequest);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{eventId}")
+                .buildAndExpand(eventId)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
