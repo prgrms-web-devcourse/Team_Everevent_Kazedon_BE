263df285c73cc44efe67f274eb1716d3bfc91f50
HeoSeungYeon <42290273+heoseungyeon@users.noreply.github.com>
2021-12-13 15:05:47 +0900

    feat: 리뷰 유효성 검사, 예외처리 및 리뷰 목록 조회 API 구현
    * feat: 리뷰 목록 조회 API 구현 및 리뷰 생성 API 수정
    
    * feat: 리뷰 유효성 검사 및 예외처리 추가
    
    * chore: convertToSimpleReview 메서드명 수정
    
    * fix: 코드리뷰 피드백 반영

--- src/main/java/kdt/prgrms/kazedon/everevent/service/ReviewService.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/service/ReviewService.java
@@ -3,14 +3,19 @@ package kdt.prgrms.kazedon.everevent.service;
 import kdt.prgrms.kazedon.everevent.domain.event.Event;
 import kdt.prgrms.kazedon.everevent.domain.event.repository.EventRepository;
 import kdt.prgrms.kazedon.everevent.domain.review.Review;
-import kdt.prgrms.kazedon.everevent.domain.review.dto.ReviewResponse;
+import kdt.prgrms.kazedon.everevent.domain.review.dto.SimpleReviewReadResponse;
 import kdt.prgrms.kazedon.everevent.domain.review.dto.ReviewWriteRequest;
+import kdt.prgrms.kazedon.everevent.domain.review.dto.SimpleReview;
 import kdt.prgrms.kazedon.everevent.domain.review.repository.ReviewRepository;
 import kdt.prgrms.kazedon.everevent.domain.user.User;
+import kdt.prgrms.kazedon.everevent.exception.ErrorMessage;
+import kdt.prgrms.kazedon.everevent.exception.NotFoundException;
 import kdt.prgrms.kazedon.everevent.service.converter.ReviewConverter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,15 +27,25 @@ public class ReviewService {
 
   private final ReviewConverter reviewConverter;
 
-  public ReviewResponse createReview(User user, Long eventId, ReviewWriteRequest request) {
+  @Transactional
+  public void createReview(User user, Long eventId, ReviewWriteRequest request) {
     Event event = eventRepository.findById(eventId)
-        .orElseThrow(
-            () -> new RuntimeException(String.format("user(id : %d) not founded", eventId)));
+        .orElseThrow(() -> new NotFoundException(ErrorMessage.EVENT_NOT_FOUNDED, eventId));
 
     Review review = reviewConverter.convertToReview(user, event, request);
     reviewRepository.save(review);
+  }
 
-    return reviewConverter.convertToReviewResponse(review);
+  @Transactional(readOnly = true)
+  public SimpleReviewReadResponse getReviews(Long eventId, Pageable pageable) {
+    Event event = eventRepository.findById(eventId)
+        .orElseThrow(() -> new NotFoundException(ErrorMessage.EVENT_NOT_FOUNDED, eventId));
+
+    Page<SimpleReview> simpleReviews = reviewRepository
+        .findByEvent(event, pageable)
+        .map(reviewConverter::convertToSimpleReview);
+
+    return reviewConverter.convertToSimpleReviewReadResponse(simpleReviews);
   }
 
 }
