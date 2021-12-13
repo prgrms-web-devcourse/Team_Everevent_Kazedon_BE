5939503a143736fca94593741ff7db19c02aaee3
Heejeong01110 <123rkdrkd@naver.com>
2021-12-13 16:43:57 +0900

    fix: 충돌 머지 해결

--- src/main/java/kdt/prgrms/kazedon/everevent/exception/ErrorMessage.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/exception/ErrorMessage.java
@@ -14,8 +14,7 @@ public enum ErrorMessage {
   DUPLICATE_EVENT_LIKE("아이디 {0}은 이미 존재하는 좋아요 항목입니다."),
   EVENTLIKE_NOT_FOUNDED("이미 삭제된 좋아요 항목입니다."),
   REVIEW_ARGUMENT_INVALID("댓글 {0} 인자의 범위가 옳지 않습니다."),
-  ARGUMENT_INVALID("인자가 유효하지 않습니다."),
-  MARKET_NOT_FOUNDED("아이디 {0}에 해당하는 가게 정보가 없습니다");
+  ARGUMENT_INVALID("인자가 유효하지 않습니다.");
 
   private final String message;
 
