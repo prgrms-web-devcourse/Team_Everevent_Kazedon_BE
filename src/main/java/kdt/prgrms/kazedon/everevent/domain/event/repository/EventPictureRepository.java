f660e3bda624b17b76160d641ad33ee4cce9c127
Heejeong Kang <48792627+Heejeong01110@users.noreply.github.com>
2021-12-19 16:10:24 +0900

    feat: 이벤트 상세조회 db 정보 반영 (#45)
    * refactor: 이벤트 상세조회 db 정보 반영
    
    * fix: eventId 삭제
    
    * fix: 사용 안하는 변수 삭제
    
    * fix: 프론트 요청사항 반영
    
    * fix: 예약어 대문자 수정

--- src/main/java/kdt/prgrms/kazedon/everevent/domain/event/repository/EventPictureRepository.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/domain/event/repository/EventPictureRepository.java
@@ -1,9 +1,14 @@
 package kdt.prgrms.kazedon.everevent.domain.event.repository;
 
+import java.util.List;
 import kdt.prgrms.kazedon.everevent.domain.event.EventPicture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventPictureRepository extends JpaRepository<EventPicture, Long> {
+
+  @Query("select e.url from EventPicture e where e.event.id = :eventId")
+  List<String> findEventPictureUrlsByEventId(Long eventId);
 }
