abc53963e628f2091804df50ca8f0594c94ef81e
HeoSeungYeon <42290273+heoseungyeon@users.noreply.github.com>
2021-12-19 14:39:48 +0900

    feat: 사업자 가게 조회 API 수정  (#43)
    * feat: 리뷰 카운트 증가 기능 구현
    
    * fix: 리뷰 카운트 증가 기능 재거
    
    * feat: 사업자 가게 조회 API 수정
    
    * fix: 코드리뷰 피드백 반영
    - MarketControlleTest 삭제
    - api/v1/markets(get) Response 변경
    - market의 reviewCount, likeCount, eventCount getter 중복 삭제
    
    * feat: /markets(get) response 데이터에 name 추가
    
    * fix: MarketServiceTest 사용되지 않는 코드 삭제

--- src/test/java/kdt/prgrms/kazedon/everevent/service/MarketServiceTest.java
+++ src/test/java/kdt/prgrms/kazedon/everevent/service/MarketServiceTest.java
@@ -5,11 +5,10 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import kdt.prgrms.kazedon.everevent.domain.market.Market;
 import kdt.prgrms.kazedon.everevent.domain.market.dto.MarketCreateRequest;
-import kdt.prgrms.kazedon.everevent.domain.market.dto.SimpleMarket;
+import kdt.prgrms.kazedon.everevent.domain.market.dto.MyMarketReadResponse;
 import kdt.prgrms.kazedon.everevent.domain.market.repository.MarketRepository;
 import kdt.prgrms.kazedon.everevent.domain.user.User;
 import kdt.prgrms.kazedon.everevent.domain.user.UserType;
@@ -20,8 +19,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
@@ -58,26 +55,11 @@ class MarketServiceTest {
             .address("test-market-address")
             .build();
 
-    private Market anotherMarket = Market.builder()
-            .user(user)
-            .name("another-test-market")
-            .description("another-test-description")
-            .address("another-test-market-address")
-            .build();
-
-    private SimpleMarket simpleMarket = SimpleMarket.builder()
+    private MyMarketReadResponse myMarket = MyMarketReadResponse.builder()
             .marketId(1L)
             .description(market.getDescription())
             .eventCount(0)
-            .favoriteCount(market.getFavoriteCount())
-            .reviewCount(0)
-            .build();
-
-    private SimpleMarket anotherSimpleMarket = SimpleMarket.builder()
-            .marketId(2L)
-            .description(anotherMarket.getDescription())
-            .eventCount(0)
-            .favoriteCount(anotherMarket.getFavoriteCount())
+            .likeCount(market.getFavoriteCount())
             .reviewCount(0)
             .build();
 
@@ -118,20 +100,15 @@ class MarketServiceTest {
     @Test
     void getMarketsByUser(){
         //Given
-        Page<Market> markets = new PageImpl<>(List.of(market, anotherMarket));
-
         when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
-        when(marketRepository.findByUser(user, pageable)).thenReturn(markets);
-        when(marketConverter.convertToSimpleMarket(market, 0, 0)).thenReturn(simpleMarket);
-        when(marketConverter.convertToSimpleMarket(anotherMarket, 0, 0)).thenReturn(anotherSimpleMarket);
+        when(marketRepository.findByUser(user)).thenReturn(Optional.of(market));
+        when(marketConverter.convertToSimpleMarket(market)).thenReturn(myMarket);
 
         //When
-        marketService.getMarketsByUser(user.getId(), pageable);
+        marketService.getMarketsByUser(user.getId());
 
         //Then
-        verify(marketRepository).findByUser(user, pageable);
-        verify(marketConverter).convertToSimpleMarket(market, 0, 0);
-        verify(marketConverter).convertToSimpleMarket(anotherMarket, 0, 0);
-        verify(marketConverter).convertToMarketReadResponse(any());
+        verify(marketRepository).findByUser(user);
+        verify(marketConverter).convertToSimpleMarket(market);
     }
 }
