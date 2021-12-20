fc793659689132f09a642c72d55acdba2b70b78f
Heejeong01110 <123rkdrkd@naver.com>
2021-12-20 18:26:59 +0900

    feat: 페이지별 접근 권한 설정

--- src/main/java/kdt/prgrms/kazedon/everevent/configures/WebSecurityConfig.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/configures/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import kdt.prgrms.kazedon.everevent.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -38,17 +39,44 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .csrf().disable()
-        .addFilter(corsFilter)
+          .addFilter(corsFilter)
         .httpBasic().disable()
         .formLogin().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .addFilter(new JwtAuthorizationFilter(authenticationManager(),jwtAuthenticationProvider()))
+        .addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtAuthenticationProvider()))
         .authorizeRequests()
-          .antMatchers("/api/v1/login").permitAll()
-          .antMatchers("/api/v1/signup/**").permitAll()
-          .antMatchers("/api/v1/favorites/**")
+          .antMatchers("/api/v1/signup")
+            .anonymous()
+          .antMatchers("/api/v1/logout")
             .access("hasRole('ROLE_USER') or hasRole('ROLE_BUSINESS')")
-          .anyRequest().permitAll();
+          .antMatchers("/api/v1/login")
+            .anonymous()
+          .antMatchers("/api/v1/likes/**")
+            .access("hasRole('ROLE_USER')")
+          .antMatchers("/api/v1/favorites/**")
+            .access("hasRole('ROLE_USER')")
+          .antMatchers("/api/v1/members/**")
+            .access("hasRole('ROLE_USER') or hasRole('ROLE_BUSINESS')")
+          .antMatchers("/api/v1/markets/*/events")
+            .access("hasRole('ROLE_BUSINESS')")
+          .antMatchers(HttpMethod.GET, "/api/v1/markets/*")
+            .access("hasRole('ROLE_USER') or hasRole('ROLE_BUSINESS')")
+          .antMatchers(HttpMethod.PATCH, "/api/v1/markets/*")
+            .access("hasRole('ROLE_BUSINESS')")
+          .antMatchers(HttpMethod.POST, "/api/v1/markets")
+            .access("hasRole('ROLE_USER')")
+          .antMatchers(HttpMethod.GET, "/api/v1/markets")
+            .access("hasRole('ROLE_BUSINESS')")
+          .antMatchers("/api/v1/events/*/participants")
+            .access("hasRole('ROLE_USER')")
+          .antMatchers(HttpMethod.POST, "/api/v1/events/*/reviews")
+            .access("hasRole('ROLE_USER')")
+          .antMatchers(HttpMethod.PATCH, "/api/v1/events/*")
+            .access("hasRole('ROLE_BUSINESS')")
+          .antMatchers(HttpMethod.POST, "/api/v1/events")
+            .access("hasRole('ROLE_BUSINESS')")
+          .anyRequest()
+            .permitAll();
   }
 
   @Override
