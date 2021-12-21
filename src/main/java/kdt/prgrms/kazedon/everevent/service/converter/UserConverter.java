90785b2d210f055c92f86effaf230d4043ee141a
Heejeong01110 <123rkdrkd@naver.com>
2021-12-21 15:01:51 +0900

    fix: 프론트 요청사항 반영

--- src/main/java/kdt/prgrms/kazedon/everevent/service/converter/UserConverter.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/service/converter/UserConverter.java
@@ -1,15 +1,25 @@
 package kdt.prgrms.kazedon.everevent.service.converter;
 
 import kdt.prgrms.kazedon.everevent.domain.user.User;
+import kdt.prgrms.kazedon.everevent.domain.user.dto.response.UserInfoResponse;
 import kdt.prgrms.kazedon.everevent.domain.user.dto.response.UserReadResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserConverter {
-    public UserReadResponse convertToUserReadResponse(User user){
+
+    public UserReadResponse convertToUserReadResponse(User user) {
         return UserReadResponse.builder()
-                .email(user.getEmail())
-                .nickname(user.getNickname())
-                .build();
+            .email(user.getEmail())
+            .nickname(user.getNickname())
+            .build();
+    }
+
+    public UserInfoResponse convertToUserInfoResponse(User user) {
+        return UserInfoResponse.builder()
+            .userId(user.getId())
+            .email(user.getEmail())
+            .nickname(user.getNickname())
+            .build();
     }
 }
