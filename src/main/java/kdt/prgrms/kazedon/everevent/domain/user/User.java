8c3a1cba1220361237ef8c63b999567ff3f60bd3
Heejeong01110 <123rkdrkd@naver.com>
2021-12-21 14:15:02 +0900

    feat: @AuthUser anonymous일때 반환값 변경

--- src/main/java/kdt/prgrms/kazedon/everevent/domain/user/User.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/domain/user/User.java
@@ -14,6 +14,7 @@ import kdt.prgrms.kazedon.everevent.domain.user.dto.request.SignUpRequest;
 import kdt.prgrms.kazedon.everevent.exception.ErrorMessage;
 import kdt.prgrms.kazedon.everevent.exception.InvalidUserArgumentException;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "user")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User extends BaseTimeEntity {
 
   @Id
