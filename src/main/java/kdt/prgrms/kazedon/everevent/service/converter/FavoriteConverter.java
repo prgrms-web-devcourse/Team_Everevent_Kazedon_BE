bd2ff7092fd1092a3ea5b4f53b9236c4874b1142
Heejeong01110 <123rkdrkd@naver.com>
2022-01-11 16:53:06 +0900

    refactor: favorite response 객체 converter로 생성

--- src/main/java/kdt/prgrms/kazedon/everevent/service/converter/FavoriteConverter.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/service/converter/FavoriteConverter.java
@@ -1,8 +1,11 @@
 package kdt.prgrms.kazedon.everevent.service.converter;
 
 import kdt.prgrms.kazedon.everevent.domain.favorite.Favorite;
+import kdt.prgrms.kazedon.everevent.domain.favorite.dto.response.SimpleMarketFavorite;
+import kdt.prgrms.kazedon.everevent.domain.favorite.dto.response.SimpleMarketFavoriteReadResponse;
 import kdt.prgrms.kazedon.everevent.domain.market.Market;
 import kdt.prgrms.kazedon.everevent.domain.user.User;
+import org.springframework.data.domain.Page;
 
 public class FavoriteConverter {
 
@@ -10,6 +13,13 @@ public class FavoriteConverter {
     return Favorite.builder()
         .user(user)
         .market(market)
+        .build();
+  }
+
+  public SimpleMarketFavoriteReadResponse convertToSimpleMarketFavoriteReadResponse(
+      Page<SimpleMarketFavorite> simpleMarketFavorites) {
+    return SimpleMarketFavoriteReadResponse.builder()
+        .markets(simpleMarketFavorites)
         .build();
   }
 }
