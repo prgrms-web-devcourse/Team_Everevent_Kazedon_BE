ef4e4bb7a98fc8593aee7ffb18b3a601706995bb
heoseungyeon <dia0312@naver.com>
2022-01-25 16:07:45 +0900

    fix: FavoriteConverter @Component 추가

--- src/main/java/kdt/prgrms/kazedon/everevent/service/converter/FavoriteConverter.java
+++ src/main/java/kdt/prgrms/kazedon/everevent/service/converter/FavoriteConverter.java
@@ -3,7 +3,9 @@ package kdt.prgrms.kazedon.everevent.service.converter;
 import kdt.prgrms.kazedon.everevent.domain.favorite.Favorite;
 import kdt.prgrms.kazedon.everevent.domain.market.Market;
 import kdt.prgrms.kazedon.everevent.domain.user.User;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FavoriteConverter {
 
   public Favorite convertToFavorite(User user, Market market) {
